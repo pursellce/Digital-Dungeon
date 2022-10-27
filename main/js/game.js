10f55278736b99d71023bb26bd8f9bdb8bb2b638
Benjamin Strange <bdstrange8@gmail.com>
2022-10-27 12:00:30 -0500

    added question token

--- main/js/game.js
+++ main/js/game.js
@@ -7,22 +7,34 @@ var myScore;
 var Frame;
 var LastX;
 var LastY;
+//var q = [];
+var questionToken = [];
+//var answered = false;
 var ladder;
 var treasure = [];
 var score = 0;
 var myUpBtn, myDownBtn, myLeftBtn, myRightBtn;
-
-
-
+var myA, myB, myC, myD; //multiple choices
+//var crashed;
+var question_count = 3;
 function startGame() {
     myGamePiece = new component(30,30,"red",450,400,"icon", "images/player.png");//30, 30, "red", 10, 120);
     //myGamePiece.gravity = 0.05;  // Commenting this out disables gravity. Will probably fully remove gravity later.
     myScore = new component("30px", "Consolas", "black", 280, 40, "text","");
-	Frame = new component("30px", "Consolas", "grey", 510, 40, "text", "");
+
+    Frame = new component("30px", "Consolas", "grey", 510, 40, "text", "");
 	myUpBtn = new component(30, 30, "blue", 50, 10);
 	myDownBtn = new component(30, 30, "blue", 50, 70);
 	myLeftBtn = new component(30, 30, "blue", 20, 40);
 	myRightBtn = new component(30, 30, "blue", 80, 40);
+    
+    //q[1].ans = "bad"
+    
+    //myA = new component(100,30, "black", 315, 403);
+	//myB = new component("30px", "black", 310, 50,"text" );
+	//myC = new component("30px", "black", 310, 90,"text");
+	//myD = new component("30px", "black", 310, 130,"text");
+    //crashed = false;
 	GenerateLevel1();
     myGameArea.start();
 }
@@ -40,12 +52,22 @@ function GenerateLevel1() {
 	// Additional walls for the level's unique design.
 	wall.push(new component(100, 100, "green", 325, 165, "icon", "images/wall.jpg"));
 	
+    for(i = 0; i < question_count; i++)//making all question tokens
+    {
+        //q.push(new component("20px", "Consolas", "grey",300,300-i*20, "question","This is the question"));
+    //q.push(new component("20px", "Consolas", "grey",200,200, "question","This is the question"));
+	    questionToken.push(new component(15,15,"gold", 400+i*40, 400-i*18,"token"));
+    }
+    for(i = 0; i < question_count;i++)//marks all crashes for tokens as false initially
+    {
+        questionToken[i].crashed = false;
+    }//questionToken.push(new component(20,20,"gold", rand()%400, rand()%400));
 	// Objects that the level has.
 	ladder = new component(30, 30, "red", 800, 400, "icon", "images/ladder.png");
 	treasure.push(new component(35, 35, "gold", 60, 100, "icon", "images/Treasure.png"));
 	treasure.push(new component(35, 35, "gold", 800, 200, "icon", "images/Treasure.png"));
 	// wall.push(new component(960, 10, "green", 100, 30, "icon", "images/Treasure.jpg"));
-	
+
 }
 
 // This section basically sets up the game window
@@ -108,21 +130,57 @@ function component(width, height, color, x, y, type, imag) {
     this.speedY = 0;    
     this.x = x;
     this.y = y;
+   
     this.imag = imag;
-    this.gravity = 0;
-    this.gravitySpeed = 0;
+    this.gravity = 0;//do not remove
+    this.gravitySpeed = 0;//do not remove
     this.update = function() {
         ctx = myGameArea.context;
         if (this.type == "text") {
             ctx.font = this.width + " " + this.height;
             ctx.fillStyle = color;
             ctx.fillText(this.text, this.x, this.y);
+        }
+        else if (this.type == "question"){//this function is no longer used
+            
+            //ctx.font = this.width + " " + this.height;
+            //ctx.fillStyle = color;
+            //if(myGamePiece.crashWith(questionToken))
+               //{ crashed = true;}
+            //if((crashed) && (this.ans != "answer"))
+            //{   
+                /*while(i < this.text.length())
+                {    
+                    ctx.fillText(this.text.substring(0,i), this.x, this.y);
+                    i++;
+                }*/
+                //ctx.fillText(this.text, this.x, this.y);
+                
+            //}
+
+            
         } 
         else if (this.type == "icon") {
             const img = new Image();//creating image
             img.src = this.imag;//to load in image 
             ctx.drawImage(img, this.x, this.y, this.width, this.height);//drawing image
-        } else {
+        } else if(this.type == "token") {//need a way to stop movement so multiple questions arent displayed
+            if(myGamePiece.crashWith(this))
+               {this.crashed = true;}
+            if(!(this.crashed)){//if not crashed display token
+                ctx.fillStyle = color;
+                ctx.fillRect(this.x, this.y, this.width, this.height);
+            }
+            else{
+                if((this.crashed) && (this.ans != "answer"))//display question as long as wrong answer
+                {   
+                    ctx.font = this.width + " " + this.height;//not sure why text is large
+                    ctx.fillStyle = color;
+                    ctx.fillText(this.text, this.x, this.y); 
+                }
+
+            }
+        }else {
             ctx.fillStyle = color;
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
@@ -199,7 +257,14 @@ function updateGameArea() {
     if (myGameArea.keys && myGameArea.keys[38]) {myGamePiece.speedY = -2; }
     if (myGameArea.keys && myGameArea.keys[40]) {myGamePiece.speedY = 2; }
 	
-	
+    //for questions
+    for(i = 0; i < question_count; i++)//needs work
+    {//when the jgame peice crahses with the token it registers input, need a way to make different answers and maybe track responses
+        if(myGamePiece.crashWith( questionToken[i])){
+            if (myGameArea.keys && myGameArea.keys[65]) { questionToken[i].ans = "answer"; }//currently only marks A as right answer
+            if (myGameArea.keys && myGameArea.keys[66]) { questionToken[i].ans = "bad"; }//b
+        }
+    }
 	// Next group of if statements are for the on screen controls.
 	if (myGameArea.x && myGameArea.y) {
     if (myUpBtn.clicked()) {
@@ -215,6 +280,22 @@ function updateGameArea() {
       myGamePiece.x += 2;
     }
   }
+
+
+  /*if (myGameArea.x && myGameArea.y) {//on screen buttons for questions
+    if (myA.clicked()) {
+       q1.ans = 1; //"answer";
+    }
+    if (myB.clicked()) {
+        q1.ans = "bad answer";
+    }
+    if (myC.clicked()) {
+        q1.ans = "bad answer";
+    }
+    if (myD.clicked()) {
+        q1.ans = "bad answer";
+    }
+  }*/
 
 	
 	
@@ -251,13 +332,14 @@ function updateGameArea() {
 	for (i = 0; i < treasure.length; i += 1) {
 		treasure[i].update();
 	}
-	
+
 	// This loop handles collision for treasure chests.
 	for (i = 0; i < treasure.length; i += 1) {
         if (myGamePiece.crashWith(treasure[i])) {
             myGamePiece.x = LastX;
 			myGamePiece.y = LastY;
 			score += 50;
+            //question.update();
         } 
     }
 	
@@ -267,11 +349,28 @@ function updateGameArea() {
 	
     Frame.text="FRAME: " + myGameArea.frameNo;
     Frame.update();
-	myScore.text="SCORE: " + score;
+    //myA.text="right";
+    //myB.text="wrong";
+    //myC.text="wrong";
+    //myD.text="wrong";
+
+    myScore.text="SCORE: " + score;
 	myScore.update();
     myGamePiece.newPos();
+
+    questionToken[0].text = "How many leading zeros does 00001101 have? Press A for 4, B for 3";
+    questionToken[1].text = "What is the logical and symbol? Press A for &&, B for ||";
+    questionToken[2].text = "What will 0010 ^ 1001 give? A for 1011, B for 1101";
+    for(i = 0; i < question_count;i++)//update all questions
+    {
+        //q[i].update();
+        questionToken[i].update();
+    }
     myGamePiece.update();
 	ladder.update();
+    //myA.update();
+    //myB.update();
+
 	myUpBtn.update();
 	myDownBtn.update();
 	myLeftBtn.update();
