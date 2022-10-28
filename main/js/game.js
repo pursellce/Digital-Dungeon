545be783aa3477908180e25719727ebf9e702627
CamronVZ <camronveazey@gmail.com>
2022-10-28 00:59:46 -0500

    Fixed Custom Font Not Working
    The website should now display the proper font on all devices now.

--- main/js/game.js
+++ main/js/game.js
@@ -297,24 +297,8 @@ function updateGameArea() {
     }
   }*/
 
-	
-	
+
     myGameArea.frameNo += 1;
-    /*if (myGameArea.frameNo == 1 || everyinterval(150)) {
-        x = myGameArea.canvas.width;
-        minHeight = 20;
-        maxHeight = 200;
-        height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
-        minGap = 50;
-        maxGap = 200;
-        gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
-        myObstacles.push(new component(10, height, "green", x, 0,"icon","images/wall.jpg"));
-        myObstacles.push(new component(10, x - height - gap, "green", x, height + gap,"icon","images/wall.jpg"));
-    }
-    for (i = 0; i < myObstacles.length; i += 1) {
-        myObstacles[i].x += -1;
-        myObstacles[i].update();
-    }*/
 
 
 	// Next two for loops are for handling collision with walls
